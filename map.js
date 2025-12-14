60e556dbc2f08efe528b9010c7692a14f0ae1cd2
satyapal626897-lgtm <satyapal626897@gmail.com>
2025-12-14 17:42:06 +0530

    Add files via upload

---
+++ map.js
@@ -0,0 +1,40 @@
+////01 map
+
+// let num=[2,8,6,9,14];
+
+// let newarr=num.map((e)=>{
+
+//     return e*2
+
+// })
+
+// console.log(newarr);
+
+/////02 filter
+
+
+// let num = [2, 4, 6, 8, 10, 15, 17];
+
+// let okk = num.filter((e) => {
+
+//     return e % 2 == 0;
+
+// })
+// console.log(okk);
+
+
+////03 reduce
+
+// let num = [2, 4, 6, 8, 10, 15, 17];
+
+// let value=num.reduce((acc,crr)=>{
+
+//     console.log(acc+crr)
+// })
+
+// console.log(value);
+
+
+
+
+
